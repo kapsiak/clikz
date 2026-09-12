feb0b486be2c94814bbc553b49192ba4b8694dba
Charlie Kapsiak <charliekapsiak@gmail.com>
2026-09-12 14:44:50 -0400

    Update scope

--- clikz/drawing/composite.lisp
+++ clikz/drawing/composite.lisp
@@ -1,7 +1,8 @@
 (in-package :clikz)
 
-(defmacro scope (args &rest body)
-  (let ((x (getf args :x 0d0))
+(defmacro with-scope (args &rest body)
+  (let ((style (getf args :style nil))
+        (x (getf args :x 0d0))
         (y (getf args :y 0d0))
         (z (getf args :z 0d0))
         (rot-x (getf args :rot-x 0d0))
@@ -12,10 +13,13 @@
                       (mat-4-rot-y ,rot-y)
                       (mat-4-rot-x ,rot-x)
                       (mat-4-translate ,x ,y ,z))
-       (with-style style
+       (with-style ,style
          ,@body))))
 
 (defun connect (a b &key arrow-start arrow-end)
   (let ((p (draw-path (list (toward a b) (toward b a)))))
     (when arrow-start (draw-path-marker  (resolve p) 0.0 :shape arrow-start))
     (when arrow-end (draw-path-marker  (resolve p) 1.0 :shape arrow-end))))
+
+
+
